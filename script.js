d2f57abff9f0ea00841d4ed8cb4b1abe1ebee8af
Kostaintyn <kostiantyn208148@student-schoolsofwestfield.org>
2024-12-18 16:55:50 +0000

    123

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+function setup() {
+    let buttons = document.getElementById("buttons");
+    let ok = document.createElement("button");
+    ok.innerHTML="ok";
+    buttons.appendChild(ok);
+    ok.addEventListener("click", checkPassword);
+    let cansel = document.createElement("button");
+    cansel.innerHTML="cansel";
+    buttons.appendChild(cansel);
+}
+
+function checkPassword() {
+    let dialogs = document.getElementById("dialogs");
+    let userName = dialogs.querySelector(':nth-child(2)').value;
+    let password = dialogs.querySelector(':nth-child(4)').value;
+    alert(userName);
+}
